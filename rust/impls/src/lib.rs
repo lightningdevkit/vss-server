b6e1ae7814259f3e42d70323667cce53c91428d5
G8XSU <3442979+G8XSU@users.noreply.github.com>
2024-09-30 13:34:00 +0900

    Add empty postgres KvStore implementation.

---
+++ rust/impls/src/lib.rs
@@ -0,0 +1,15 @@
+//! Hosts VSS protocol compliant [`KvStore`] implementations for various backends.
+//!
+//! VSS is an open-source project designed to offer a server-side cloud storage solution specifically
+//! tailored for noncustodial Lightning supporting mobile wallets. Its primary objective is to
+//! simplify the development process for Lightning wallets by providing a secure means to store
+//! and manage the essential state required for Lightning Network (LN) operations.
+//!
+//! [`KvStore`]: api::kv_store::KvStore
+
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(missing_docs)]
+
+/// Contains [PostgreSQL](https://www.postgresql.org/) based backend implementation for VSS.
+pub mod postgres_store;
