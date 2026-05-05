5d905684dfd4cca561f4358532c634df61f62baf
Leo Nash <hello@leonash.net>
2026-05-05 23:48:46 +0000

    Read `vss.proto` from the local repository instead of github
    We now read `vss.proto` from the local repository instead of dowloading
    it from a previous commit on github.
    
    We also update the checked-in `types.rs` file to match the one generated
    with the `genproto` flag, and now skip any formatting of this
    auto-generated file.

--- rust/api/build.rs
+++ rust/api/build.rs
@@ -1,7 +1,7 @@
 #[cfg(genproto)]
 extern crate prost_build;
 #[cfg(genproto)]
-use std::{env, fs, fs::File, path::Path};
+use std::{env, fs, path::Path};
 
 /// To generate updated proto objects, run `RUSTFLAGS="--cfg genproto" cargo build`
 fn main() {
@@ -11,10 +11,8 @@ fn main() {
 
 #[cfg(genproto)]
 fn generate_protos() {
-	download_file(
-		"https://raw.githubusercontent.com/lightningdevkit/vss-server/7f492fcac0c561b212f49ca40f7d16075822440f/app/src/main/proto/vss.proto",
-		"src/proto/vss.proto",
-	).unwrap();
+	fs::create_dir_all("src/proto").unwrap();
+	fs::copy("../../proto/vss.proto", "src/proto/vss.proto").unwrap();
 
 	prost_build::Config::new()
 		.bytes(&["."])
@@ -23,13 +21,4 @@ fn generate_protos() {
 	println!("OUT_DIR: {}", &env::var("OUT_DIR").unwrap());
 	let from_path = Path::new(&env::var("OUT_DIR").unwrap()).join("vss.rs");
 	fs::copy(from_path, "src/types.rs").unwrap();
-}
-
-#[cfg(genproto)]
-fn download_file(url: &str, save_to: &str) -> Result<(), Box<dyn std::error::Error>> {
-	let mut response = reqwest::blocking::get(url)?;
-	fs::create_dir_all(Path::new(save_to).parent().unwrap())?;
-	let mut out_file = File::create(save_to)?;
-	response.copy_to(&mut out_file)?;
-	Ok(())
 }
